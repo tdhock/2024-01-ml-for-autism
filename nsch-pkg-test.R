ab01dbb0d487fc47f4e7a3dba793b23752a505fb
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-02-08 07:49:44 -0500

    base pattern

--- nsch-pkg-test.R
+++ nsch-pkg-test.R
@@ -39,12 +39,13 @@ get_year <- function(year_url, data.dir=tempdir()){
   }
   url.dt <- nc::capture_all_str(
     data.dir.year.html,
-    url="//.*?topical_Stata[.]zip")
+    url=list(
+      "//.*?",
+      base="[^/]*topical_Stata[.]zip"))
   if(nrow(url.dt)==0)
     stop("no topical_Stata.zip urls on ", year_url)
   http_url <- paste0("http:", url.dt$url)
-  year.zip <- basename(http_url)
-  data.dir.year.zip <- file.path(data.dir, year.zip)
+  data.dir.year.zip <- file.path(data.dir, url.dt$base)
   if(!file.exists(data.dir.year.zip)){
     download.file(http_url, data.dir.year.zip)
   }
